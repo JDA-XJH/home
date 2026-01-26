079d1974334c679206b02f6babf245385984bfa5
夜影、NightShadow <xiaojiehuang2011@gmail.com>
2026-01-26 13:17:22 +0100

    update

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -6,7 +6,7 @@
         <span class="name">
           {{ siteName }}
         </span>
-        <span class="tip"> 加载需5-10秒~ </span>
+        <span class="tip"> 请稍等片刻～ 加载需5-10秒～ </span>
       </div>
     </div>
     <div class="loader-section section-left" />
