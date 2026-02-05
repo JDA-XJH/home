e6d335ab6a05ed6aa2dfbf7c33a5ee9b421ddeb9
XJH <2324huangx@iesjoandaustria.org>
2026-02-05 19:03:32 +0100

    是否是壁紙問題

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -40,15 +40,19 @@ const bgRandom = Math.floor(Math.random() * 12 + 1);
 const changeBg = (type) => {
     if (type == 0) {
     if (mainUrl) {
-  bgUrl.value = "https://bing.img.run/uhd.php";
+      bgUrl.value = "https://bing.img.run/uhd.php";
+} else {
+      bgUrl.value = "https://api.dujin.org/bing/1920.php"; 
     }
+  } else {
+    bgUrl.value = "https://api.timelessq.com/bing/random";
   }
 };
-//    } else if (type == 1) {
+//     else if (type == 1) {
 //    bgUrl.value = "https://bing.img.run/uhd.php";
 //  } else if (type == 2) {
 //    bgUrl.value = "https://bing.img.run/rand_uhd.php";
-//  } else if (type == 3) {
+//  } else if (type == 3) 
 //    bgUrl.value = "https://api.timelessq.com/bing/random";
 
 // const changeBg = (type) => {
